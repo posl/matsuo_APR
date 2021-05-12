2bbab89a6bff36bc62d8f542832a5f51b86727a7
松尾 春紀 <haruki.matsuo110676@e-seikatsu.co.jp>
2021-05-12 11:31:09 +0900

    08-14テスト作成&演習1問題文作成

--- test/08-14/main.c
+++ test/08-14/main.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
 #include <math.h>
 
-/*f(x)の出力*/
-void function()
-{
-    printf("f(x) = pow(x,2) - 2\n");
-}
-
 /*f(x)*/
-double f(double x)
+double f(double x,double a,double b,double c)
 {
-    return pow(x,2) - 2;
+    return a * pow(x,2) + b * x + c;
 }
 
 /*f(x)の導関数*/
-double g(double x)
+double g(double x,double a,double b)
 {
-    return 2 * x;
+    return 2 * a * x + b;
 }
 
 int main()
@@ -24,31 +18,39 @@ int main()
     //利用する変数
     double x, new_x, eps;
     int number,i;
+    double a,b,c;
+
+    //fxの取り込み
+    //printf("a : ");
+    scanf("%lf",&a);
+    //printf("b : ");
+    scanf("%lf",&b);
+    //printf("c : ");
+    scanf("%lf",&c);
 
     //データの取り込み
-    function();
-    printf("初期値 : ");
+    //printf("初期値 : ");
     scanf("%lf",&x);
-    printf("計算精度 : ");
+    //printf("計算精度 : ");
     scanf("%lf",&eps);
-    printf("繰り返し上限回数 : ");
+    //printf("繰り返し上限回数 : ");
     scanf("%d",&number);
-    printf("繰り返し\tnew x\t\tf(x)\t\tg(x)\n");
+    //printf("繰り返し\tnew x\t\tf(x)\t\tg(x)\n");
     
     /* ニュートン法による解の導出 */
     for(i=0;i<number;i++){
-        new_x = x - f(x) / g(x);
+        new_x = x - f(x,a,b,c) / g(x,a,b);
         /* 収束条件で判定し、解を出力 */
-        if(fabs(new_x - x) < eps && (g(x) > 0.0001)){
-            printf("x = %f\n", new_x);
+        if(fabs(new_x - x) < eps && (g(x,a,b) > 0.0001)){
+            printf("%f\n", new_x);
             break;
         }
 
-         printf("%2d\t\t%f\t%f\t%f\n", i, new_x, f(x), g(x));
+        //printf("%2d\t\t%f\t%f\t%f\n", i, new_x, f(x,a,b,c), g(x,a,b));
 
          /* 収束条件、重解の判定をし、重解を出力 */
-        if((fabs(new_x - x) < eps) && (g(x) <= 0.0001)){
-            printf("x = %f(重解)\n", new_x);
+        if((fabs(new_x - x) < eps) && (g(x,a,b) <= 0.0001)){
+            printf("%f(重解)\n", new_x);
             break;
         }
         x = new_x;
